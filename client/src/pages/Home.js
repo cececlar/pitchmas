e442dc56bc5d743278bcd45fb755b9a2041f31b4
cececlar <cclaridge@brainstation.io>
2021-11-14 15:14:14 -0500

    display predictive text options in client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CreateMovie from "../components/CreateMovie/CreateMovie";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:8080";
 
@@ -21,13 +20,39 @@ export default function Home() {
     socket.on("options", (data) => {
       data = data.split("\n");
       data.pop();
-      console.log(data);
+      setOptions(data);
     });
   }, []);
   return (
     <div>
       <h2>I am the Home page.</h2>
-      <CreateMovie addMovie={addMovie} />
+      <form onSubmit={addMovie} className="new-movie__form">
+        <label className="new-movie__label" htmlFor="title">
+          Title
+        </label>
+        <input
+          className="new-movie__input"
+          type="text"
+          name="title"
+          id="title"
+        ></input>
+        <label className="new-movie__label" htmlFor="overview">
+          Overview
+        </label>
+        <input
+          className="new-movie__input"
+          type="textarea"
+          name="overview"
+          id="overview"
+        ></input>
+        <button className="new-movie__button" type="submit">
+          Submit
+        </button>
+      </form>
+      {options.length > 0 &&
+        options.map((option) => {
+          return <p>{option}</p>;
+        })}
     </div>
   );
 }
