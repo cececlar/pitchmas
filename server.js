502ed63d5240d03b73224f383bfc270d46a0a5e9
cececlar <cclaridge@brainstation.io>
2021-12-08 14:51:06 -0500

    update middleware to serve static files in production

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use(morgan("dev"));
 app.use("/api/movies", movieRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+  app.use(express.static("./client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
+  });
 }
 
 io.on("connection", (socket) => {
